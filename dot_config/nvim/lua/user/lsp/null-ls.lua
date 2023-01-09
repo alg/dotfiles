71dac01666d07c7182a2f5c958fa7c0008b1cc74
Aleksey Gureiev <agureiev@shakuro.com>
2023-01-09 19:57:39 +0300

    Added autoformatting to null-ls

--- dot_config/nvim/lua/user/lsp/null-ls.lua
+++ dot_config/nvim/lua/user/lsp/null-ls.lua
@@ -19,23 +19,12 @@ local diagnostics = null_ls.builtins.diagnostics
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup {
-    -- you can reuse a shared lspconfig on_attach callback here
-    -- on_attach = function(client, bufnr)
-    --     if client.supports_method("textDocument/formatting") then
-    --         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    --         vim.api.nvim_create_autocmd("BufWritePre", {
-    --             group = augroup,
-    --             buffer = bufnr,
-    --             callback = function()
-    --                 vim.lsp.buf.format({ bufnr = bufnr })
-    --             end,
-    --         })
-    --     end
-    -- end,
+    on_attach = require("user.lsp.format").on_attach,
 
     sources = {
-        formatting.black.with({ extra_args = { "--fast" } }),
-        diagnostics.flake8,
+        formatting.yapf,
+        -- formatting.black.with({ extra_args = { "--fast" } }),
+        -- diagnostics.flake8,
         formatting.golines.with({ extra_args = { "-m", "120" }}),
         -- null_ls.builtins.code_actions.gitsigns,
     }
