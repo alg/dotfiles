c34bbd711f7974d711bf4ff8bb0892162c876358
Aleksey Gureev <spyromus@noizeramp.com>
2023-01-23 21:03:00 +0300

    [Fish] Deleted

--- dot_config/private_fish/conf.d/abbr.fish
+++
@@ -1,9 +0,0 @@
-# Time warrior
-
-abbr tw timew
-abbr twm timew worklog :month
-abbr tws timew summary :ids
-
-# Lazy git
-
-abbr lg lazygit
--- dot_config/private_fish/config.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-    fish_add_path ~/.rbenv/shims
-
-    # Commands to run in interactive sessions can go here
-end
