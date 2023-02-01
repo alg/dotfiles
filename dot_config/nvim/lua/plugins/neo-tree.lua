ef41a627d3d3bb97fb0bbe57b8bf5d8d6556a95d
Aleksey Gureev <spyromus@noizeramp.com>
2023-02-01 19:49:37 +0300

    [nvim] Configured neo tree icons; removed nvim-tree

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -9,8 +9,12 @@ return {
         -- for support of "_with_window_picker" suffix in actions
         "s1n7ax/nvim-window-picker",
     },
+
     config = function()
-        vim.g.neo_tree_remove_legacy_commands = 1
+        vim.fn.sign_define("DiagnosticSignError", {text = " ", texthl = "DiagnosticSignError"})
+        vim.fn.sign_define("DiagnosticSignWarn", {text = " ", texthl = "DiagnosticSignWarn"})
+        vim.fn.sign_define("DiagnosticSignInfo", {text = " ", texthl = "DiagnosticSignInfo"})
+        vim.fn.sign_define("DiagnosticSignHint", {text = "", texthl = "DiagnosticSignHint"})
 
         require('neo-tree').setup {
             window = {
@@ -19,6 +23,45 @@ return {
                     ['<leader>e'] = 'close_window',
                     ['<cr>'] = 'open_with_window_picker',
                     ['<c-v>'] = 'vsplit_with_window_picker',
+                },
+            },
+
+            filesystem = {
+                follow_current_file = true,
+            },
+
+            renderers = {
+                directory = {
+                    { "indent" },
+                    { "icon" },
+                    { "current_filter" },
+                    {
+                        "container",
+                        width = "100%",
+                        right_padding = 0,
+                        content = {
+                            { "name", zindex = 10 },
+                            { "clipboard", zindex = 10 },
+                            { "diagnostics", errors_only = true, zindex = 20, align = "right" },
+                        },
+                    },
+                },
+                file = {
+                    { "indent" },
+                    { "icon" },
+                    {
+                        "container",
+                        width = "100%",
+                        right_padding = 0,
+                        content = {
+                            { "name", zindex = 10 },
+                            { "clipboard", zindex = 10 },
+                            { "bufnr", zindex = 10 },
+                            { "modified", zindex = 20, align = "right" },
+                            { "diagnostics",  zindex = 20, align = "right" },
+                            { "git_status", zindex = 20, align = "right" },
+                        },
+                    },
                 },
             },
         }
