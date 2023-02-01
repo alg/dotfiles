b870ea70b420e81746a49ea57ed584d7ca7263df
Aleksey Gureev <spyromus@noizeramp.com>
2023-02-01 19:25:35 +0300

    [nvim] Added window picker to neo-tree

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -2,9 +2,12 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
     dependencies = { 
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        
+        -- for support of "_with_window_picker" suffix in actions
+        "s1n7ax/nvim-window-picker",
     },
     config = function()
         vim.g.neo_tree_remove_legacy_commands = 1
@@ -14,6 +17,8 @@ return {
                 mappings = {
                     ['<bs>'] = 'close_node',
                     ['<leader>e'] = 'close_window',
+                    ['<cr>'] = 'open_with_window_picker',
+                    ['<c-v>'] = 'vsplit_with_window_picker',
                 },
             },
         }
---
+++ dot_config/nvim/lua/plugins/window-picker.lua
@@ -0,0 +1,22 @@
+return {
+    "s1n7ax/nvim-window-picker",
+    config = function()
+        require'window-picker'.setup({
+            autoselect_one = true,
+            include_current = false,
+            show_prompt = false,
+            user_winbar = 'always',
+            filter_rules = {
+                -- filter using buffer options
+                bo = {
+                    -- if the file type is one of following, the window will be ignored
+                    filetype = { "neo-tree", "neo-tree-popup", "notify" },
+
+                    -- if the buffer type is one of following, the window will be ignored
+                    buftype = { "terminal", "quickfix" },
+                },
+            },
+            other_win_hl_color = "#e35e4f",
+        })
+    end,
+}
