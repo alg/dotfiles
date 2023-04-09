483ebc9cd7bc5ce11dfe249c7317de6761984758
Aleksey Gureiev <agureiev@shakuro.com>
2023-04-09 12:00:10 +0300

    [nvim] replaced solargraph with diagnostics rubocop / reek

--- dot_config/nvim/lua/user/lsp/init.lua
+++ dot_config/nvim/lua/user/lsp/init.lua
@@ -1,6 +1,6 @@
 local ok, lspconfig = pcall(require, "lspconfig")
 if not ok then
-    return
+	return
 end
 
 local format = require("user.lsp.format")
@@ -10,28 +10,21 @@ local keymaps = require("user.lsp.keymaps")
 keymaps.config_global_keymaps()
 
 local on_attach = function(client, bufnr)
-  keymaps.on_attach(client, bufnr)
-  format.on_attach(client, bufnr)
+	keymaps.on_attach(client, bufnr)
+	format.on_attach(client, bufnr)
 end
 
-lspconfig.pyright.setup{
-    on_attach = on_attach,
-}
+lspconfig.pyright.setup({
+	on_attach = on_attach,
+})
 
-lspconfig.gopls.setup {
-    on_attach = on_attach,
-}
-
-lspconfig.solargraph.setup {
-    on_attach = keymaps.on_attach,
-	  flags = {
-	      debounce_text_changes = 150,
-	  },
-}
+lspconfig.gopls.setup({
+	on_attach = on_attach,
+})
 
 local ok, mason = pcall(require, "mason")
 if ok then
-  mason.setup {}
+	mason.setup({})
 end
 
 require("user.lsp.null-ls")
--- dot_config/nvim/lua/user/lsp/null-ls.lua
+++ dot_config/nvim/lua/user/lsp/null-ls.lua
@@ -1,32 +1,36 @@
 local ok, null_ls = pcall(require, "null-ls")
 if not ok then
-    return
+	return
 end
 
 local masonok, mason_null_ls = pcall(require, "mason-null-ls")
 if masonok then
-	mason_null_ls.setup {
+	mason_null_ls.setup({
 		automatic_setup = {
 			types = {
-				rubocop = { 'formatting' },
-			}
-		}
-	}
+				rubocop = { "formatting" },
+			},
+		},
+	})
 end
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-null_ls.setup {
-    on_attach = require("user.lsp.format").on_attach,
+null_ls.setup({
+	debug = true,
+	on_attach = require("user.lsp.format").on_attach,
 
-    sources = {
-        formatting.black,
-        diagnostics.ruff,
-        formatting.golines.with({ extra_args = { "-m", "120" }}),
-    }
-}
+	sources = {
+		formatting.black,
+		diagnostics.ruff,
+		formatting.golines.with({ extra_args = { "-m", "120" } }),
+		formatting.rubocop,
+		diagnostics.rubocop,
+		diagnostics.reek,
+	},
+})
 
 if masonok then
 	mason_null_ls.setup_handlers()
