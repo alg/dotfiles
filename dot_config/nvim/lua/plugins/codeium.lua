0116550fb4a4565bc6a0447269fe9c334c7a5891
Aleksey Gureiev <agureiev@shakuro.com>
2023-06-22 23:21:05 +0300

    [nvim] added codeium

---
+++ dot_config/nvim/lua/plugins/codeium.lua
@@ -0,0 +1,9 @@
+return {
+    'Exafunction/codeium.vim',
+    config = function()
+        vim.keymap.set('i', '<M-k>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+        vim.keymap.set('i', '<M-j>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+        vim.keymap.set('i', '<M-l>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+        vim.keymap.set('i', '<M-,>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+    end
+}
