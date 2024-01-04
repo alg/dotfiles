d181bfe6be3d36c00f047c4fcb503de79cab0d5a
Aleksey Gureiev <spyromus@noizeramp.com>
2024-01-04 23:28:40 +0300

    [nvim] Next / prev todo on ]t / [t

--- dot_config/nvim/lua/plugins/todo-comments.lua
+++ dot_config/nvim/lua/plugins/todo-comments.lua
@@ -5,4 +5,8 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = {},
+    keys = {
+        { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+        { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+    }
 }
