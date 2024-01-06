38e483d122641a05acc17930b4c17029370a6e32
Aleksey Gureiev <agureiev@shakuro.com>
2024-01-06 20:33:23 +0300

    [zsh] Fixed zoxide and lazy nvm/node/npm

---
+++ dot_config/zsh/conf.d/nvm.zsh
@@ -0,0 +1,22 @@
+# Lazy NVM
+lazynvm() {
+  unset -f nvm node npm
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+}
+
+nvm() {
+  lazynvm
+  nvm $@
+}
+
+node() {
+  lazynvm
+  node $@
+}
+
+npm() {
+  lazynvm
+  npm $@
+}
+
