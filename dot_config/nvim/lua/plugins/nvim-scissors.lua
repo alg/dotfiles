1bd8356f4e89713ee892f6e68a15f131b8059c6f
Aleksey Gureiev <spyromus@noizeramp.com>
2024-02-03 23:51:31 +0300

    [nvim] Added mapping desc to nvim-scissors

--- dot_config/nvim/lua/plugins/nvim-scissors.lua
+++ dot_config/nvim/lua/plugins/nvim-scissors.lua
@@ -8,6 +8,12 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>se", function() require("scissors").editSnippet() end)
         vim.keymap.set({ "n", "x" }, "<leader>sa", function() require("scissors").addNewSnippet() end)
+
+        local has_miniclue, miniclue = pcall(require, "mini.clue")
+        if has_miniclue then
+            miniclue.set_mapping_desc('n', '<Leader>se', 'Edit snippet')
+            miniclue.set_mapping_desc('n', '<Leader>sa', 'Add snippet')
+        end
     end
 }
 
